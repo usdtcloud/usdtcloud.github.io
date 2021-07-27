1db245cfa407f4cd399d89abb43e28c7bbed312d
usdtcloud <usdtcloud@gmail.com>
2021-07-27 10:45:25 +0800

    修复部分bug
    Signed-off-by: usdtcloud <usdtcloud@gmail.com>

--- README.md
+++
@@ -1,7 +0,0 @@
-# ipioo.github.io
-
-
-
-### vendor
-
-各种css库以及Js的引入文件
--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    // jQuery twittie
-	$('#tweet').twittie({
-        count: 2,
-        template: '<i class="fa fa-twitter"></i> {{tweet}}'
-    });
-    
-});
-
